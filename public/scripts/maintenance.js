7a0ad54639229e33d77c8928cb6f0020ca7b04bf
Firepup Sixfifty <firepyp650@gmail.com>
2023-10-01 22:41:02 -0500

    TODO: Use env var for maintenance checks

--- public/scripts/maintenance.js
+++ public/scripts/maintenance.js
@@ -1,12 +1,12 @@
-maintenance = false
+maintenance = false //TODO: Use env var instead
 if (maintenance) {
   if (window.location.href.indexOf("maintenance") == -1) {
     document.getElementById("Maintenance").innerHTML = `<span class="warning">Warning:<br/>Site under maintenance.<br/>Be warned, site may be unstable.</span>`
   }
   document.getElementById("Maintenance").removeAttribute("hidden")
-  if (window.location.href.indexOf("maintenance") == -1 && window.location.href.indexOf('id') == -1) {
-    window.location.href = "/maintenance"
-}
+  //if (window.location.href.indexOf("maintenance") == -1 && window.location.href.indexOf('id') == -1) {
+    //window.location.href = "/maintenance"
+  //}
 } else if (!(window.location.href.indexOf("maintenance") == -1)) {
   document.getElementById("Maintenance").innerHTML = `<span class="rainbow">Website not under maintenance.</span>`
 }
